47e87e39646fad5ee26c2e3ef4c0fb40475b12b1
britbroet <brittanybroetje@gmail.com>
2016-09-20 08:55:14 -0700

    todo list assignment

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+
+ 	$( function() {
+   		$( "#sortable").sortable();
+    	$( "#sortable").disableSelection();
+ 	});
+
+
+	$("#addItem").click(function() {
+		var newItem = $('input[name=textField]').val();
+			if (newItem.length > 0) {
+			$('ul').append('<li>' + newItem + '<img src="../img/delete.png" class="trash">' + '</li>'); 
+			$('#textField').val("");
+			$('li .trash').click(function() {
+				$(this).parent('li').fadeOut(300);
+			});
+		}
+	});
+
+
+//keeps from resetting list after add (since using img input type)
+
+	$("#addItem").click(function(event){
+    event.preventDefault();
+	});
+
+   
+
+});
