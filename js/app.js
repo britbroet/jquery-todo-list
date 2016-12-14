7c65d29103038910a39c9075dae7dac85852ce76
britbroet <brittanybroetje@gmail.com>
2016-12-14 07:25:33 -0800

    fix move img url

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	$("#addItem").click(function() {
 		var newItem = $('input[name=textField]').val();
 			if (newItem.length > 0) {
-			$('ul').append('<li>' + '<img src="../img/move.png" class="move">' + newItem + '<img src="img/delete.png" class="trash">' + '</li>'); 
+			$('ul').append('<li>' + '<img src="img/move.png" class="move">' + newItem + '<img src="img/delete.png" class="trash">' + '</li>'); 
 			$('#textField').val("");
 			$('li .trash').click(function() {
 				$(this).parent('li').fadeOut(300);
