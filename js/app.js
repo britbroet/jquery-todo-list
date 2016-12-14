56f2821985648a134df0c38038f7d52f3c08b96a
britbroet <brittanybroetje@gmail.com>
2016-12-14 08:38:49 -0800

    mobile functionality

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
  	});
 
 
-	$("#addItem").click(function() {
+	$("#addItem").on('touchstart click', function() {
 		var newItem = $('input[name=textField]').val();
 			if (newItem.length > 0) {
-			$('ul').append('<li><div class="row todoItem">' + '<div class="col-xs-1"><img src="img/move.png" class="move"></div>' + '<div class="col-xs-9">' + newItem + '</div>' + '<div class="col-xs-1"><img src="img/delete.png" class="trash"></div>' + '</li></div>'); 
+			$('ul').append('<li><div class="row todoItem">' + '<div class="hidden-xs col-sm-1"><img src="img/move.png" class="move"></div>' + '<div class="col-xs-10 col-sm-9">' + newItem + '</div>' + '<div class="col-xs-1"><img src="img/delete.png" class="trash"></div>' + '</li></div>'); 
 			$('#textField').val("");
-			$('li .trash').click(function() {
-				$(this).parent('li').fadeOut(300);
+			$('li .trash').on('touchstart click', function() {
+				$(this).parent().parent().parent().fadeOut(300);
 			});
 		}
 	});
