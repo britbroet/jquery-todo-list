f9e618aa5d7a5fe413bbc47aaa91780ca7d36470
britbroet <brittanybroetje@gmail.com>
2016-12-14 08:08:41 -0800

    responsiveness

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	$("#addItem").click(function() {
 		var newItem = $('input[name=textField]').val();
 			if (newItem.length > 0) {
-			$('ul').append('<li>' + '<img src="img/move.png" class="move">' + newItem + '<img src="img/delete.png" class="trash">' + '</li>'); 
+			$('ul').append('<li><div class="row todoItem">' + '<div class="col-xs-1"><img src="img/move.png" class="move"></div>' + '<div class="col-xs-9">' + newItem + '</div>' + '<div class="col-xs-1"><img src="img/delete.png" class="trash"></div>' + '</li></div>'); 
 			$('#textField').val("");
 			$('li .trash').click(function() {
 				$(this).parent('li').fadeOut(300);
